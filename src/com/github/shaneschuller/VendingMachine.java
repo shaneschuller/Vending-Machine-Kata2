380b95fa7ae80d15ba82412958b9767e2d574ba0
Shane Schuller <shane.schuller@gmail.com>
2015-07-24 02:06:04 -0400

    Now doesn't accept pennies. Moderate code refactor

--- src/com/github/shaneschuller/VendingMachine.java
+++ src/com/github/shaneschuller/VendingMachine.java
@@ -1,10 +1,16 @@
 package com.github.shaneschuller;
 import com.github.shaneschuller.Coin;
+
+import java.util.HashMap;
+
 /**
  * Created by Shane on 7/24/2015.
  */
 public class VendingMachine {
-    private static String display = "0";
+    private static final String INSERT_COIN = "INSERT COIN";
+    private static final String NUMBER_FORMAT_SAFE_DEFAULT_STRING = "0";
+    private static String display = INSERT_COIN;
+    private static HashMap<Coin, Integer> balance;
 
     public static String display()
     {
@@ -15,7 +21,14 @@ public class VendingMachine {
     {
         try
         {
-            display = "" + (Double.parseDouble(display) + coin.value());
+            if(coin == Coin.DIME || coin == Coin.NICKEL || coin == Coin.QUARTER)
+            {
+                if(display.equals(INSERT_COIN))
+                {
+                    display = NUMBER_FORMAT_SAFE_DEFAULT_STRING;
+                }
+                display = "" + (Double.parseDouble(display) + coin.value());
+            }
         } catch (NumberFormatException nfe)
         {
             System.out.println("Display is not a number");
@@ -24,6 +37,20 @@ public class VendingMachine {
 
     public static void returnMoney()
     {
-        display = "0";
+        display = INSERT_COIN;
+    }
+
+    public static String getBalance()
+    {
+        double balanceInNumeric = 0.0;
+        balanceInNumeric += balance.get(Coin.QUARTER) * Coin.QUARTER.value();
+        balanceInNumeric += balance.get(Coin.DIME) * Coin.DIME.value();
+        balanceInNumeric += balance.get(Coin.NICKEL) * Coin.NICKEL.value();
+        return ""+balanceInNumeric;
+    }
+
+    public static void setBalance(HashMap<Coin, Integer> bal)
+    {
+        balance = bal;
     }
 }
--- tst/VendingMachineKataTestMain.java
+++ tst/VendingMachineKataTestMain.java
@@ -1,7 +1,11 @@
 import com.github.shaneschuller.VendingMachine;
 import com.github.shaneschuller.Coin;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
+
+import java.util.HashMap;
 
 /**
  * Created by Shane on 7/23/2015.
@@ -9,6 +13,26 @@ import org.junit.Assert;
 
 public class VendingMachineKataTestMain
 {
+    private double balance;
+    @Before
+    public void setBalanceOfVendingMachine()
+    {
+        HashMap<Coin, Integer> initBalance = new HashMap<Coin,Integer>();
+        initBalance.put(Coin.QUARTER, 20);
+        balance += Coin.QUARTER.value() * 20;
+        initBalance.put(Coin.DIME, 20);
+        balance += Coin.DIME.value() * 20;
+        initBalance.put(Coin.NICKEL, 60);
+        balance += Coin.NICKEL.value() * 60;
+        VendingMachine.setBalance(initBalance);
+    }
+
+    @After
+    public void setTestingBalanceToZero()
+    {
+        balance = 0.0;
+    }
+
     @Test
     public void acceptsQuarters()
     {
@@ -31,5 +55,13 @@ public class VendingMachineKataTestMain
         VendingMachine.insertCoin(Coin.DIME);
         Assert.assertEquals("0.1", VendingMachine.display());
         VendingMachine.returnMoney();
+    }
+
+    @Test
+    public void doesNotAcceptPennies()
+    {
+        VendingMachine.insertCoin(Coin.PENNY);
+        Assert.assertEquals("INSERT COIN", VendingMachine.display());
+        Assert.assertEquals(""+balance, VendingMachine.getBalance());
     }
 }
