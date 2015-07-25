b2a369592d5e3866978e0348d8474b0f422c3b13
Shane Schuller <shane.schuller@gmail.com>
2015-07-24 23:40:22 -0400

    Did some refactoring to the test to make adding coins easier. Also added correct change test

--- src/com/github/shaneschuller/VendingMachine.java
+++ src/com/github/shaneschuller/VendingMachine.java
@@ -11,10 +11,12 @@ public class VendingMachine {
     private static final String SUCCESSFUL_PURCHASE = "THANK YOU";
     private static final String UNSUCCESSFUL_PURCHASE = "PRICE";
     private static final String SOLD_OUT = "SOLD OUT" ;
+    private static final String ZERO_AMOUNT = "0.00";
     private static String display = INSERT_COIN;
-    private static HashMap<Coin, Integer> balance = new HashMap<Coin, Integer>();
-    private static HashMap<Coin, Integer> amount = new HashMap<Coin, Integer>();
+    private static HashMap<Coin, Integer> balance = new HashMap<>();
+    private static HashMap<Coin, Integer> amount = new HashMap<>();
     private static HashMap<Selection, Integer> supply = new HashMap<>();
+    private static Double coinReturn = 0.0;
 
     static
     {
@@ -52,13 +54,50 @@ public class VendingMachine {
         }
     }
 
-    public static void returnMoney()
+    public static String returnMoney()
     {
         amount.put(Coin.QUARTER, 0);
         amount.put(Coin.DIME, 0);
         amount.put(Coin.NICKEL, 0);
         display = INSERT_COIN;
+        return ZERO_AMOUNT;
     }
+
+    private static void makeChange(Selection purchase)
+    {
+        double amountOfChange = Math.round((getAmountValue() - purchase.value()) * 100.0) / 100.0;
+        boolean needToMakeChange = amountOfChange >= 0 ? true : false;
+        //This section determines if we actually need to make change
+        int quarters = needToMakeChange? getAmountOfCoins(amountOfChange, Coin.QUARTER.value()) : 0;
+        amountOfChange -= quarters * Coin.QUARTER.value();
+        int dimes = needToMakeChange ? getAmountOfCoins(amountOfChange, Coin.DIME.value()) : 0;
+        amountOfChange -= dimes * Coin.DIME.value();
+        int nickles = needToMakeChange ? getAmountOfCoins(amountOfChange, Coin.NICKEL.value()) : 0;
+        amountOfChange -= nickles * Coin.NICKEL.value();
+
+        //This section actually processes the change and dispenses it to the coinReturn
+        coinReturn += Coin.QUARTER.value() * quarters;
+        coinReturn += Coin.DIME.value() * dimes;
+        coinReturn += Coin.NICKEL.value() * nickles;
+    }
+
+
+    /**
+     * This is a helper function that will convert the Doubles to int's so we can get the number of times we must loop.
+     * This is because i am storing the values as decimals less than 0 to represent their actual dollar amount
+     * @param costOfPurchase - this is the current amount remaining to make the purchase
+     * @param coinValue - this is the value of the coin we are mod'ing by.
+     * @return this integer will be the number of times we loop. In other words how many of the coins do we put in the
+     * machine.
+     */
+    private static Integer getAmountOfCoins(Double costOfPurchase, Double coinValue)
+    {
+        return  (
+                ((Double)(costOfPurchase * 100)).intValue() / ((Double)(coinValue * 100)).intValue()
+        );
+
+    }
+
 
     public static String getBalance()
     {
@@ -77,6 +116,7 @@ public class VendingMachine {
     {
         if(getAmountValue() >= purchase.value() && supply.get(purchase) != 0)
         {
+            makeChange(purchase);
             addAmountToBalance();
             return SUCCESSFUL_PURCHASE;
         }
@@ -112,5 +152,10 @@ public class VendingMachine {
         {
             supply.putAll(updateSupply);
         }
+    }
+
+    public static String coinReturn()
+    {
+        return ""+coinReturn;
     }
 }
--- tst/VendingMachineKataTestMain.java
+++ tst/VendingMachineKataTestMain.java
@@ -19,7 +19,7 @@ public class VendingMachineKataTestMain
     @Before
     public void setBalanceOfVendingMachine()
     {
-        HashMap<Coin, Integer> initBalance = new HashMap<Coin,Integer>();
+        HashMap<Coin, Integer> initBalance = new HashMap<>();
         initBalance.put(Coin.QUARTER, 20);
         balance += Coin.QUARTER.value() * 20;
         initBalance.put(Coin.DIME, 20);
@@ -28,7 +28,7 @@ public class VendingMachineKataTestMain
         balance += Coin.NICKEL.value() * 60;
         VendingMachine.setBalance(initBalance);
 
-        completeSupply = new HashMap<Selection,Integer>();
+        completeSupply = new HashMap<>();
         completeSupply.put(Selection.CANDY, 10);
         completeSupply.put(Selection.CHIPS, 10);
         completeSupply.put(Selection.COLA, 10);
@@ -75,10 +75,7 @@ public class VendingMachineKataTestMain
     @Test
     public void canPurchaseCola()
     {
-        VendingMachine.insertCoin(Coin.QUARTER);
-        VendingMachine.insertCoin(Coin.QUARTER);
-        VendingMachine.insertCoin(Coin.QUARTER);
-        VendingMachine.insertCoin(Coin.QUARTER);
+        enterEnoughMoney(Selection.COLA.value());
         Assert.assertEquals("1.0", VendingMachine.display());
         Assert.assertEquals("THANK YOU", VendingMachine.purchase(Selection.COLA));
     }
@@ -86,8 +83,7 @@ public class VendingMachineKataTestMain
     @Test
     public void canPurchaseChips()
     {
-        VendingMachine.insertCoin(Coin.QUARTER);
-        VendingMachine.insertCoin(Coin.QUARTER);
+        enterEnoughMoney(Selection.CHIPS.value());
         Assert.assertEquals("0.5", VendingMachine.display());
         Assert.assertEquals("THANK YOU", VendingMachine.purchase(Selection.CHIPS));
     }
@@ -95,11 +91,7 @@ public class VendingMachineKataTestMain
     @Test
     public void canPurchaseCandy()
     {
-
-        VendingMachine.insertCoin(Coin.QUARTER);
-        VendingMachine.insertCoin(Coin.QUARTER);
-        VendingMachine.insertCoin(Coin.DIME);
-        VendingMachine.insertCoin(Coin.NICKEL);
+        enterEnoughMoney(Selection.CANDY.value());
         Assert.assertEquals("0.65", VendingMachine.display());
         Assert.assertEquals("THANK YOU", VendingMachine.purchase(Selection.CANDY));
 
@@ -134,10 +126,81 @@ public class VendingMachineKataTestMain
         Assert.assertEquals("SOLD OUT", VendingMachine.purchase(Selection.COLA));
     }
 
+    @Test
+    public void correctChangeIsMade()
+    {
+        enterEnoughMoney(0.75);
+        VendingMachine.purchase(Selection.CANDY);
+        Assert.assertEquals("0.1", VendingMachine.coinReturn());
+    }
+
+    @Test
+    public void returnMoneyShowsZeroOnDisplay()
+    {
+        VendingMachine.insertCoin(Coin.DIME);
+        Assert.assertEquals("0.00", VendingMachine.returnMoney());
+    }
+
     /* CONSTRUCTOR */
     public VendingMachineKataTestMain()
     {
         super();
+    }
+
+      /* Private Helper Functions */
+
+    /**
+     * This function will build the test up be entering the correct amount of money needed to make the purchase.
+     * @param purchaseValue - this is the Selection Enum value we will compare against and get the value of.
+     */
+    private void enterEnoughMoney(Double purchaseValue)
+    {
+        double costOfPurchase = purchaseValue;
+        if(costOfPurchase > 0)
+        {
+            putCoinsInTheMachine(convertPriceToInteger(costOfPurchase, Coin.QUARTER.value()), Coin.QUARTER);
+            costOfPurchase -= convertPriceToInteger(costOfPurchase, Coin.QUARTER.value()) * Coin.QUARTER.value();
+        }
+        if(costOfPurchase > 0)
+        {
+            putCoinsInTheMachine(convertPriceToInteger(costOfPurchase, Coin.DIME.value()), Coin.DIME);
+            costOfPurchase -= convertPriceToInteger(costOfPurchase, Coin.DIME.value()) * Coin.DIME.value();
+        }
+        if(costOfPurchase > 0)
+        {
+            putCoinsInTheMachine(convertPriceToInteger(costOfPurchase, Coin.NICKEL.value()), Coin.NICKEL);
+            //This next line only exists so that we could add more coins later if we so chose, I.E Pennies, or Pesos, or etc
+            costOfPurchase -= convertPriceToInteger(costOfPurchase, Coin.NICKEL.value()) * Coin.NICKEL.value();
+        }
+    }
+
+    /**
+     * This is a helper function that will convert the Doubles to int's so we can get the number of times we must loop.
+     * This is because i am storing the values as decimals less than 0 to represent their actual dollar amount
+     * @param costOfPurchase - this is the current amount remaining to make the purchase
+     * @param coinValue - this is the value of the coin we are mod'ing by.
+     * @return this integer will be the number of times we loop. In other words how many of the coins do we put in the
+     * machine.
+     */
+    private Integer convertPriceToInteger(Double costOfPurchase, Double coinValue)
+    {
+        return  (
+                ((Double)(costOfPurchase * 100)).intValue() / ((Double)(coinValue * 100)).intValue()
+        );
+
+    }
+
+    /**
+     * This function actually does the loop of putting the coins in the machine.
+     * @param v - number of times we are going to loop
+     * @param coin - type of coin we are inserting.
+     */
+    private void putCoinsInTheMachine(Integer v, Coin coin)
+    {
+        for(int i = v; i > 0; i--)
+        {
+            VendingMachine.insertCoin(coin);
+        }
     }
 
 }
